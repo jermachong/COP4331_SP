c6c5b691e24a5d0df61c03a4a930847912ad23e5
Yessenia Diaz <yesseniadiaz915@gmail.com>
2025-01-22 17:32:29 -0500

    Working UpdateContacts.php

--- LAMPAPI/UpdateContacts.php
+++ LAMPAPI/UpdateContacts.php
@@ -1,11 +1,28 @@
+
+
 <?php
 	$inData = getRequestInfo();
+
+	header('Access-Control-Allow-Origin: *');
+	header("Content-Type: application/json");
+	header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+	header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+	error_reporting(-1); // reports all errors
+	ini_set("display_errors", "1"); // shows all errors
+	ini_set("log_errors", 1);
+	ini_set("error_log", "/tmp/php-error.log");
+
+	if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+		header("HTTP/1.1 200 OK");
+		exit(0);
+	}
 	
-	$phone = $inData["phone"];
-	$email = $inData["email"];
-	$newFirst = $inData["newFirstName"];
-	$newLast = $inData["newLastName"];
-	$id = $inData["id"];
+	$phone = $inData["Phone"];
+	$email = $inData["Email"];
+	$newFirst = $inData["newFirst"];
+	$newLast = $inData["newLast"];
+	$id = $inData["UserID"];
 	
 
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
@@ -15,8 +32,8 @@
 	} 
 	else
 	{
-		$stmt = $conn->prepare("UPDATE Contacts SET FirstName = ?, LastName=?, PhoneNumber= ?, EmailAddress= ? WHERE ID= ?");
-			$stmt->bind_param("ssssi", $newFirst, $newLast, $phoneNumber, $emailAddress, $id);
+		$stmt = $conn->prepare("UPDATE Contacts SET FirstName = ?, LastName=?, Phone= ?, Email= ? WHERE ID= ?");
+			$stmt->bind_param("ssssi", $newFirst, $newLast, $phone, $email, $id);
 			$stmt->execute();
 
 			$stmt->close();
