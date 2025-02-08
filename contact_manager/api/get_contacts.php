4d8aeb46d64671d7b61a96deac517457538ae256
Khoi Nguyen <khoinguy9@gmail.com>
2025-02-07 22:28:35 -0500

    fix get_contact php naming

--- contact_manager/api/get_contacts.php
+++ contact_manager/api/get_contacts.php
@@ -4,7 +4,8 @@ include '../config/db.php';
 header("Content-Type: application/json");
 
 $user_id = $_GET['user_id'];
-$sql = "SELECT * FROM contacts WHERE user_id = $user_id";
+$sql = "SELECT * FROM contacts WHERE User_id = $user_id";
+
 $result = $conn->query($sql);
 
 $contacts = [];
