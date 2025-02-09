78ccbb0d146d9b2600f60dae76c8c83ab9d417aa
Khoi Nguyen <khoinguy9@gmail.com>
2025-02-08 19:14:55 -0500

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -291,18 +291,26 @@ function cancelEdit(
   if (!rowDiv) return;
 
   rowDiv.innerHTML = `
-    <div class="p-2 firstName">${originalFirst}</div>
-    <div class="p-2 lastName">${originalLast}</div>
-    <div class="p-2 email">${originalEmail}</div>
-    <div class="p-2 phone">${originalPhone}</div>
-    <div class="p-2 d-flex gap-2">
-      <button class="btn btn-outline-light btn-sm edit-btn" data-contact-id="${contactID}" onclick="editContact(${contactID})">
-        <i class="bi bi-pencil-square" style="color: #ffffff"></i>
-      </button>
-      <button class="btn btn-danger btn-sm delete-btn" data-contact-id="${contactID}">
-        <i class="bi bi-trash-fill"></i>
-      </button>
-    </div>
+          <div id="hstackContainer" class="w-100" style="border-color: #ffffff;">
+        <div class="d-flex justify-content-around" style="width: 100%; gap: 0;">
+          <div class="p-2 firstName" style="color: #ffffff; flex: 2; text-align: left;">${originalFirst}</div>
+          <div class="p-2 lastName"  style="color: #ffffff; flex: 2; text-align: left;">${originalFirst}</div>
+          <div class="p-2 email"     style="color: #ffffff; flex: 3; text-align: left;">${originalEmail}</div>
+          <div class="p-2 phone"     style="color: #ffffff; flex: 2; text-align: left;">${originalPhone}</div>
+          <div class="p-2 d-flex align-items-center" style="flex: 1; gap: 5px; justify-content: center;">
+            <button class="btn btn-outline-light btn-sm edit-btn" data-contact-id="${contactID}" style="color: #ffffff;">
+             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+              <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293z"/>
+            </svg>
+            </button>
+            <button class="btn btn-danger btn-sm delete-btn" data-contact-id="${contactID}" style="color: #ffffff;">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+              <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"/>
+            </svg>
+            </button>
+          </div>
+        </div>
+      </div>
   `;
   rowDiv.setAttribute("data-contact-id", contactID);
 }
